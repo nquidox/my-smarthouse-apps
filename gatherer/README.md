c7cd8e74820ceed17850de1a6f2d75b93abae585
nquidox <nquidox@gmail.com>
2024-03-01 18:39:59 +0300

    Switched to postgresql db

--- gatherer/README.md
+++ gatherer/README.md
@@ -1,3 +1,8 @@
 # Smarthouse micropython - Gatherer
 
-A script that gathers data from sensors via mqtt and stores it in sqlite3 database.
+A script that gathers data from sensors via mqtt and stores it in postgresql database.
+
+Fill in connection params for database connection.
+Edit gatherer.service file if you want to use it with systemd.
+
+In case if psycopg2 doesn't install from req.txt, install it manually with pip install psycopg2-binary
--- gatherer/gatherer.py
+++ gatherer/gatherer.py
@@ -1,30 +1,10 @@
 import ast
-import os
-import sqlite3
 import time
+import psycopg2
 import paho.mqtt.subscribe as subscribe
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB = os.path.join(BASE_DIR, 'database.db')
-
-broker = "hostname"
-topic = "topic"
-
-
-def db_worker(op: str, sql: str, values: tuple = None) -> None:
-    db = sqlite3.Connection(database=DB)
-    cursor = db.cursor()
-
-    match op:
-        case "init":
-            cursor.execute(sql)
-
-        case "insert":
-            cursor.execute(sql, values)
-
-    db.commit()
-    cursor.close()
-    db.close()
+broker = "192.168.0.201"
+topic = "esp32/banya"
 
 
 def get_data_from_broker() -> list:
@@ -37,23 +17,28 @@ def get_data_from_broker() -> list:
         pass
 
 
-def main() -> None:
+def main():
+    conn = psycopg2.connect(
+        database="",
+        user="",
+        password="",
+        host="",
+        port="5432"
+    )
+    cursor = conn.cursor()
+
     while True:
-        data = get_data_from_broker()
-        for item in data:
-            sql = '''INSERT INTO sensors(hex_id, temp_value, seconds, date_time) VALUES (?, ?, ?, ?)'''
-            values = (item["id"], item["temp"], time.time(), time.strftime("%d-%m-%Y %H:%M:%S", time.localtime()))
-            db_worker("insert", sql, values)
+        for item in get_data_from_broker():
+            cursor.execute(
+                "INSERT INTO bathhouse_sensors(hex_id, temp_value, seconds, date_time) VALUES (%s, %s, %s, %s)",
+                (item["id"], item["temp"], time.time(), time.strftime("%d-%m-%Y %H:%M:%S", time.localtime()))
+            )
+        conn.commit()
         time.sleep(1)
 
+    cursor.close()
+    conn.close()
 
 if __name__ == "__main__":
-    db_worker("init", '''CREATE TABLE IF NOT EXISTS sensors(
-                "id" INTEGER,
-                "hex_id" TEXT,
-                "temp_value" REAL,
-                "seconds" REAL,
-                "date_time" TEXT,
-                PRIMARY KEY ("id" AUTOINCREMENT)
-            )''')
     main()
+
