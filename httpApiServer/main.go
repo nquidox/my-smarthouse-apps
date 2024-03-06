8ce7131d654baff20c64f1ca1aba7c10cf630824
nquidox <nquidox@gmail.com>
2024-03-06 19:08:06 +0300

    hour sensors data

--- httpApiServer/main.go
+++ httpApiServer/main.go
@@ -32,7 +32,7 @@ func getValues() ([]byte, error) {
 		FROM bathhouse_sensors bs
 		INNER JOIN sensors_aliases sa ON bs.hex_id=sa.hex_id
 		GROUP BY sa.name;
-`)
+		`)
 
 	if err != nil {
 		log.Fatal("Cannot read from database: ", err)
@@ -49,15 +49,67 @@ func getTempJSON(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getHourValues() ([]byte, error) {
+	type dbRecord struct {
+		TempValue float64 `db:"temp_value"`
+		Seconds   float64 `db:"seconds"`
+		DateTime  string  `db:"date_time"`
+	}
+
+	type hourData struct {
+		Name   string
+		Params []dbRecord
+	}
+
+	var data []dbRecord
+	var output []hourData
+	var sensors []string
+
+	err := DB.Select(&sensors, `SELECT name FROM sensors_aliases`)
+
+	if err != nil {
+		log.Fatal("Cannot read from database: ", err)
+	}
+	fmt.Println(sensors)
+
+	for _, sensor := range sensors {
+		err = DB.Select(&data, `
+		SELECT bs.temp_value, bs.seconds, bs.date_time
+		FROM bathhouse_sensors bs
+		LEFT JOIN sensors_aliases sa on sa.hex_id=bs.hex_id
+		WHERE sa.name=$1
+		LIMIT 60
+		`, sensor)
+
+		if err != nil {
+			log.Fatal("Cannot read from database: ", err)
+		}
+		output = append(output, hourData{sensor, data})
+	}
+
+	return json.Marshal(output)
+}
+
+func getHourData(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := getHourValues()
+		fmt.Fprintf(w, string(response))
+	}
+}
+
 func main() {
 	CONFIG, _ = conf.LoadConfig(".")
 	DB = db.Connection(CONFIG.Database.Driver, CONFIG.Database.DbPath)
 
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello! Have a nice day.")
 	})
+
 	mux.HandleFunc("/bathhouse", getTempJSON)
+	mux.HandleFunc("/hour", getHourData)
 
 	addr := CONFIG.Server.Hostname + ":" + strconv.Itoa(int(CONFIG.Server.Port))
 	log.Fatal(http.ListenAndServe(addr, mux))
